1c7f76f3aefd68dd2cf356df28a24766cdb08f01
NarjisseAjlil <narjisse.ajlil@gmail.com>
2025-07-02 11:30:33 +0200

    add firelaser

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,27 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    [SerializeField] GameObject laser;
 
     void Update()
     {
         Vector2 move = Vector2.zero;
 
-        if (Keyboard.current.upArrowKey.isPressed)
-            move.y += 1;
-        if (Keyboard.current.downArrowKey.isPressed)
-            move.y -= 1;
+      
         if (Keyboard.current.leftArrowKey.isPressed)
             move.x -= 1;
         if (Keyboard.current.rightArrowKey.isPressed)
             move.x += 1;
 
         transform.position += (Vector3)move.normalized * moveSpeed * Time.deltaTime;
+  
+  
+  if (Input.GetButtonDown("FireLaser"))
+        {
+            Instantiate(laser, transform.position, Quaternion.identity);
+        }
+
+          
+
     }
 }
