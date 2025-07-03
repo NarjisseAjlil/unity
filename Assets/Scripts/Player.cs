924cfff0f2c3916671efe7880e3cdc2ce1358372
NarjisseAjlil <narjisse.ajlil@gmail.com>
2025-07-04 01:04:52 +0200

    comments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,16 +14,16 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        // Récupérer position souris
+        
         Vector3 mousePos = Input.mousePosition;
         Vector3 worldPos = mainCam.ScreenToWorldPoint(new Vector3(mousePos.x, mainCam.nearClipPlane + 10f));
 
-        // Seulement l’axe Y
+       
         Vector3 newPosition = transform.position;
         newPosition.x = Mathf.Lerp(transform.position.x, worldPos.x, moveSpeed * Time.deltaTime);
         transform.position = newPosition;
 
-        // Tir laser
+      
         if (Input.GetButtonDown("FireLaser"))
         {
             Instantiate(laser, transform.position, Quaternion.identity);
